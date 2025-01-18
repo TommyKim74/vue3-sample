277188c720c2e95b2a6dc3f99d70d9947d0f1f86
TommyKim74 <kims197411@gmail.com>
2025-01-18 11:56:25 +0900

    new created files

---
+++ vue3/define-model/InputEmail.vue
@@ -0,0 +1,31 @@
+<!-- InputEmail.vue -->
+<template>
+	<div class="input-wrapper">
+		<input
+			type="text"
+			class="input-email"
+			:class="{ error: !emailChk && validChkBox }"
+			:placeholder="placeholder"
+			v-model="email"
+		/>
+	</div>
+</template>
+<script setup>
+import { ref, defineModel } from 'vue';
+const email = defineModel('email', {
+	default: '',
+});
+const emailChk = defineModel('emailChk', {
+	default: true,
+});
+
+defineProps({
+	placeholder: {
+		type: String,
+		default: '이메일 주소 입력',
+	},
+	// ...생략
+});
+// 이하 생략
+</script>
+
---
+++ vue3/define-model/Parent.vue
@@ -0,0 +1,13 @@
+<!-- Parent.vue -->
+<template>
+    <InputEmail
+        placeholder="이메일주소"
+        v-model:email="email"
+        v-model:emailChk="emailChk"
+    />
+</template>
+<script setup>
+import InputEmail from '@/component/InputEmail.vue'
+const email = ref(''); // 이메일 값
+const emailChk = ref(false); // 이메일 유효성 검사 결과
+</script>
